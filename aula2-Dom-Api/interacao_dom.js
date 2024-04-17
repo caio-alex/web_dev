2290a3d503c4577f71c1220788a46898c71d6067
caio <caio62858@gmail.com>
2024-04-17 20:02:23 -0300

    tanananaanananana tanann

--- aula2-Dom-Api/interacao_dom.js
+++ aula2-Dom-Api/interacao_dom.js
@@ -1,7 +1,6 @@
 var n1 = document.getElementById('n1')
 var n2 = document.getElementById('n2')
 var r = document.getElementById('r')
-var re = document.getElementById('re')
 
 function soma(){
     var resultado = n1.valueAsNumber + n2.valueAsNumber
@@ -14,9 +13,9 @@ function soma(){
 function sub(){
     var resultado = n1.valueAsNumber - n2.valueAsNumber
     if(isNaN(resultado))
-       re.textContent = 'Você não digitou nada'
+       r.textContent = 'Você não digitou nada'
     else
-         re.textContent = resultado
+         r.textContent = resultado
     
 }
 function mult(){
