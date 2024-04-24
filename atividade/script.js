3d065ca6375e2756d8bf00cafcd009a75e078397
caio <caio62858@gmail.com>
2024-04-24 11:22:03 -0300

    ex1

---
+++ atividade/script.js
@@ -0,0 +1,11 @@
+let contador = 0;
+let res = document.querySelector('section#cliques')
+function contar(){
+    contador++;
+    res.innerHTML = `<p>O contador está com ${contador} cliques</p>`;
+}
+
+function zerar(){
+    contador =0;
+    res.innerHTML = null;
+}
